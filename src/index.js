f7fddd1f35003952d7c29cfad67c154f17742d19
Laura <laura.fv8197@gmail.com>
2025-03-27 19:51:05 +0100

    Fix: useRoutes

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import BookDetail from './BookDetail'; // cuando lo tengas hecho
+import BookDetail from './BookDetail';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
+    <Router>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/book/:id" element={<BookDetail />} />
       </Routes>
+    </Router>
   </React.StrictMode>
 );
 
