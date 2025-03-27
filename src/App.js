b395b7ceef202ad6ceb5b81e14a4105015c992ec
Laura <laura.fv8197@gmail.com>
2025-03-27 15:11:50 +0100

    react/java-api-done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import { Card, ListGroup, Col, Row, Container } from "react-bootstrap";
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 function App() {
+  
+  const [books, setBooks] = useState([])
+  
+  useEffect(() => {
+    fetch(process.env.REACT_APP_API_URL)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Error en la API: ${response.statusText}`);
+      }
+      return response.json();
+    })
+      .then((data) => {
+        // console.log("Datos recibidos:", data);
+        setBooks(data);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container mx-auto row p-4'>
+    {/* <Container> */}
+    <h1>Books</h1>
+    {books.map(book => (
+      <div key={book.id} className='col-sm-2 col-md-6 col-lg-3'>
+        <Card style={{ width: '18rem', maxHeight: '50rem'}} className='shadow p-3 mb-5 bg-body-tertiary'>
+          <Card.Img variant="top" src={book.image} alt={book.title} width="150" />
+          <Card.Body>
+            <Card.Title className='truncate-title'>{book.title}</Card.Title>
+            <Card.Text className='text-break text-wrap truncate-description'>{book.description}</Card.Text>
+          </Card.Body>
+          <ListGroup className="list-group-flush">
+            <ListGroup.Item>{book.author}</ListGroup.Item>
+          </ListGroup>
+          <Card.Body>
+            <Card.Link href={book.link}>Buy Link</Card.Link>
+            <Card.Link as={Link} to={`/book/${book.id}`}>More info</Card.Link>
+          </Card.Body>
+          </Card>
+        </div>
+      ))}
     </div>
+    
   );
+
+
+  // return (
+  //   <div className='container mx-auto w-90 p-4'>
+  //     <h1>Books</h1>
+  //     <div className='container row'>
+  //       {books.map(book => (
+  //         <div key={book.id} className='col-4'>
+  //           <h2>{book.title}</h2>
+  //           <p>{book.author}</p>
+  //           <img src={book.image} alt={book.title} width="150" />
+  //         </div>
+  //       ))}
+
+  //     </div>
+  //   </div>
+  // );
 }
 
 export default App;
---
+++ src/BookDetail.js
@@ -0,0 +1,61 @@
+import './App.css';
+import { AiTwotoneBook } from "react-icons/ai";
+import { FaEuroSign } from "react-icons/fa";
+import { Card } from "react-bootstrap";
+import { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom'
+
+
+function BookDetail() {
+    const [book, setBook] = useState(null)
+    const {id}= useParams()
+
+      useEffect(() => {
+        fetch(`${process.env.REACT_APP_API_URL}/${id}`)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(`Error en la API: ${response.statusText}`);
+          }
+          return response.json();
+        })
+          .then((data) => {
+            setBook(data);
+          })
+          .catch((error) => console.error("Error fetching data:", error));
+      }, []);
+
+    return (
+        <div className='container m-auto w-80 justify-center p-4'>
+        <h1 className='d-flex justify-content-center text-decoration-underline fw-bold'>Book Detail</h1>
+        {book != null ? (
+        <div key={book.id} className='row d-flex justify-content-center'>
+            <h2 className='d-flex justify-content-center p-4 fw-light fst-italic'>{book.title}</h2>
+            <div className='col-lg-6 col-xl-3'>
+                <img src={book.image} alt={book.title} width="300" />
+                <h3 className='fw-light'>{book.author}</h3>
+            </div>
+            <div className='col-lg-6 col-xl-6'>
+                <p className='fs-4'>{book.description}</p>
+            </div>
+            <div className='d-flex justify-content-center'>
+                <p className='fs-5'><FaEuroSign />{book.price}</p>
+                <p className='fs-5 px-4'><AiTwotoneBook /> {book.pages} pages</p>
+            </div>
+            <div className='d-flex w-auto justify-content-center'>
+                <Card.Link href={book.link} className='p-3 btn btn-primary'>Buy Link</Card.Link>
+                <Link to="/"><button className='mx-4 p-3 btn btn-outline-primary'>Back to Home</button></Link>
+            </div>
+        </div>
+            ) : (
+            <div className="text-center mt-5">
+                <h2>Book not found</h2>
+                <Link to="/">
+                <button className='mx-4 p-3 btn btn-outline-primary'>Back to Home</button>
+                </Link>
+            </div>
+            )}
+        </div>
+    )
+}
+
+export default BookDetail;
