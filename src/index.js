ded1ccd50877f2a5d3d7a18ee452a482c8efddc4
Laura <laura.fv8197@gmail.com>
2025-03-27 19:46:59 +0100

    Fix: router nesting issue and clean React routing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -10,12 +11,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/book/:id" element={<BookDetail />} />
       </Routes>
-    </BrowserRouter>
   </React.StrictMode>
 );
 
